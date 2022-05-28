757b2a9c6da90903810bb14fa92521f85cb4b0de
Nikita Tomashevich <quant3234@gmail.com>
2022-05-28 11:40:44 +0300

    Player name from user name

--- lobby.gd
+++ lobby.gd
@@ -28,11 +28,11 @@ func _ready():
 	
 	$Connect/ItemList.add_item("REM T4 Lex Aeterna")
 	# Set the player name according to the system username. Fallback to the path.
-	#if OS.has_environment("USERNAME"):
-	#	$Connect/Name.text = OS.get_environment("USERNAME")
-	#else:
-	#	var desktop_path = OS.get_system_dir(0).replace("\\", "/").split("/")
-	#	$Connect/Name.text = desktop_path[desktop_path.size() - 2]
+	if OS.has_environment("USERNAME"):
+		$Connect/Name.text = OS.get_environment("USERNAME")
+	else:
+		var desktop_path = OS.get_system_dir(0).replace("\\", "/").split("/")
+		$Connect/Name.text = desktop_path[desktop_path.size() - 2]
 
 
 func _on_host_pressed():
